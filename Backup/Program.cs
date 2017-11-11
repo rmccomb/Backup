dd3c7b60d8a64657cf230790de340fa32bbba748
Robert McComb <robertmccomb@msn.com>
2017-11-11 17:36:40 +1000

    add

--- Backup/Program.cs
+++ Backup/Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Backup.Logic;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -20,14 +23,39 @@ namespace Backup
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            Initialise();
 
             // Show the system tray icon.
             using (ProcessIcon pi = new ProcessIcon())
             {
                 pi.Display();
-
-                // Make sure the application runs!
                 Application.Run();
+            }
+        }
+
+        private static void Initialise()
+        {
+            try
+            {
+                var path = ConfigurationManager.AppSettings[FileManager.SettingsKey];
+                if (string.IsNullOrEmpty(path))
+                {
+                    path = Directory.GetParent(
+                        Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)).FullName;
+                    if (Environment.OSVersion.Version.Major >= 6)
+                    {
+                        path = Directory.GetParent(path).ToString();
+                    }
+
+                    path = Path.Combine(path, FileManager.SettingsFolder);
+                    if (!Directory.Exists(path))
+                        Directory.CreateDirectory(path);
+                }
+                FileManager.Initialise(path);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("There was a problem with the initialisation - check settings", ex);
             }
         }
     }

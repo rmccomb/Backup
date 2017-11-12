407e69fd0acbb1a781659477f58b250e97877cd6
Robert McComb <robertmccomb@msn.com>
2017-11-12 11:07:19 +1000

    add missing

---
+++ Backup.Logic/Source.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backup.Logic
+{
+    public class Source
+    {
+        public string Directory;
+        public string Pattern;
+
+        public Source(string path, string pattern)
+        {
+            this.Directory= path;
+            this.Pattern = pattern;
+        }
+    }
+}

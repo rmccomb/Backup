8203f8079bf30e8e984f08e2125b3a4d7fa407ad
Robert McComb <robertmccomb@msn.com>
2017-11-20 14:26:53 +1000

    UI tweeking

---
+++ Backup/MessageForm.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Backup
+{
+    partial class MessageForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MessageForm));
+            this.Message = new System.Windows.Forms.Label();
+            this.Yes = new System.Windows.Forms.Button();
+            this.No = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Message
+            // 
+            this.Message.AutoSize = true;
+            this.Message.Location = new System.Drawing.Point(13, 13);
+            this.Message.Name = "Message";
+            this.Message.Size = new System.Drawing.Size(55, 13);
+            this.Message.TabIndex = 0;
+            this.Message.Text = "[message]";
+            // 
+            // Yes
+            // 
+            this.Yes.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.Yes.Location = new System.Drawing.Point(190, 66);
+            this.Yes.Name = "Yes";
+            this.Yes.Size = new System.Drawing.Size(75, 23);
+            this.Yes.TabIndex = 1;
+            this.Yes.Text = "Yes";
+            this.Yes.UseVisualStyleBackColor = true;
+            this.Yes.Click += new System.EventHandler(this.Yes_Click);
+            // 
+            // No
+            // 
+            this.No.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.No.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.No.Location = new System.Drawing.Point(271, 66);
+            this.No.Name = "No";
+            this.No.Size = new System.Drawing.Size(75, 23);
+            this.No.TabIndex = 2;
+            this.No.Text = "No";
+            this.No.UseVisualStyleBackColor = true;
+            this.No.Click += new System.EventHandler(this.No_Click);
+            // 
+            // MessageForm
+            // 
+            this.AcceptButton = this.Yes;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.No;
+            this.ClientSize = new System.Drawing.Size(358, 101);
+            this.Controls.Add(this.No);
+            this.Controls.Add(this.Yes);
+            this.Controls.Add(this.Message);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MessageForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "MessageForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Message;
+        private System.Windows.Forms.Button Yes;
+        private System.Windows.Forms.Button No;
+    }
+}
---
+++ Backup/MessageForm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Backup
+{
+    public partial class MessageForm : Form
+    {
+        public MessageForm(string message, string heading)
+        {
+            InitializeComponent();
+
+            this.Message.Text = message;
+            this.Text = heading;
+        }
+
+        private void No_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.No;
+        }
+
+        private void Yes_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Yes;
+        }
+    }
+}

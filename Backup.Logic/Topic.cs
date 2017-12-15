35b17125803d74f52eafe8c8ac8f12701196ee86
Robert McComb <mccombenator@gmail.com>
2017-12-15 13:58:25 +1000

    topic

---
+++ Backup.Logic/Topic.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Backup.Logic
+{
+    [Serializable]
+    class Topic
+    {
+        public string TopicARN { get; set; }
+        public string JobId { get; set; }
+        public string QueueARN { get; set; }
+        public string QueueUrl { get; internal set; }
+    }
+}

b9a91f7524dfbdda26034ac8b06f58c131b07194
Robert McComb <mccombenator@gmail.com>
2018-01-03 10:51:10 +1000

    Modified only switch

---
+++ Backup.Logic/Settings.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Backup.Logic
+{
+    [Serializable]
+    public class Settings
+    {
+        string fileSystemDirectory;
+        public string FileSystemDirectory
+        {
+            get
+            { if (String.IsNullOrEmpty(fileSystemDirectory))
+                    return Path.Combine(FileManager.GetTempDirectory(), FileManager.ArchiveFolder);
+                else
+                    return fileSystemDirectory;
+            }
+            set { this.fileSystemDirectory = value; }
+        }
+        public bool IsFileSystemEnabled { get; set; }
+        public string AWSS3Bucket { get; set; }
+        public bool IsS3BucketEnabled { get; set; }
+
+        public bool IsGlacierEnabled { get; set; }
+        public string AWSGlacierVault { get; set; }
+
+        public string AWSAccessKeyID { get; set; }
+        public string AWSSecretAccessKey { get; set; }
+
+        public bool CreateBackupOnStart { get; set; }
+        public AWSRegionEndPoint AWSS3Region { get; set; }
+        public AWSRegionEndPoint AWSGlacierRegion { get; set; }
+
+        public string SMSContact { get; set; }
+        public bool IsSMSContactEnabled { get; set; }
+    }
+}
